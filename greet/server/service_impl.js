dc93cf124d6d5d20b4a055d1f4b2eda2b82a95e2
cezzre <git@cezz.re>
2022-05-21 14:16:33 +0100

    Add GreetManyTimes RPC

--- greet/server/service_impl.js
+++ greet/server/service_impl.js
@@ -9,3 +9,16 @@ exports.greet = (call, callback) => {
 
   callback(null, res);
 };
+
+exports.greetManyTimes = (call) => {
+  console.log("GreetManyTimes was invoked");
+
+  const res = new pb.GreetResponse();
+
+  for (let i = 0; i < 10; ++i) {
+    res.setResult(`Hello ${call.request.getFirstName()} - number ${i}`);
+    call.write(res);
+  }
+
+  call.end();
+};
